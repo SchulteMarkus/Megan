a380b67fd52d5dfed8c8461146d79d9de5e03ba2
Markus Schulte <markusschulte83@gmail.com>
2014-10-20 13:56:23 +0200

    new 'toList' helper.

--- src/main/java/com/megan/helper/ListHelper.java
+++ src/main/java/com/megan/helper/ListHelper.java
@@ -1,12 +1,27 @@
 package com.megan.helper;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.neo4j.conversion.Result;
 
 public final class ListHelper {
+
+	public static <T> List<T> toList(final Iterator<T> iter) {
+		final List<T> list = new ArrayList<>();
+		while (iter.hasNext()) {
+			list.add(iter.next());
+		}
+
+		return list;
+	}
+
+	public static <T> List<T> toList(final Result<T> result) {
+		return ListHelper.toList(result.iterator());
+	}
 
 	public static <T> Set<T> toSet(final Iterator<T> iter) {
 		final Set<T> set = new HashSet<>();
